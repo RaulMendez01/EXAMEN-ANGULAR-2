529597b2dddc90e3300732d0bb20d218c5f346ca
RaulMendez01 <rmendeza6@gmail.com>
2021-11-07 17:10:33 -0600

    commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const path = require ('path');
 const express = require ('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/Nuevo'));
+app.use(express.static(__dirname + '/dist/examen'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/Nuevo/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/examen/index.html'));
 });
 
 app.listen(process.env.PORT ||5000,()=>{  console.log("iniciado")});
